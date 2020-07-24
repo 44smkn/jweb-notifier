0872416a4b59f0316540dbef93669ea2ebd581e1
Kenji Shimizu <jrywm121@gmail.com>
2020-07-24 18:45:17 +0900

    passwordの値オブジェクトを追加

---
+++ domain/model/user/password.go
@@ -0,0 +1,19 @@
+package user
+
+import (
+	"errors"
+	"regexp"
+)
+
+type Password struct {
+	password string
+}
+
+var charTypeRe = regexp.MustCompile(`^[a-zA-Z0-9]{8,}$`)
+
+func NewPassword(password string) (*Password, error) {
+	if !charTypeRe.MatchString(password) {
+		return nil, errors.New("英数字8文字以上ではありません")
+	}
+	return &Password{password}, nil
+}
--- domain/model/user/user.go
+++ domain/model/user/user.go
@@ -1,8 +1,8 @@
 package user
 
 type User struct {
-	UserId       Id
-	Password     string
+	UserId       *Id
+	Password     *Password
 	Notification string
 	WatchList    []string
 }
