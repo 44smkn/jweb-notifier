a143177d03dea4f00dea61af6a11aeff8fadded6
Kenji Shimizu <jrywm121@gmail.com>
2020-08-13 01:05:38 +0900

    メソッドの引数の型の参照先を修正

--- domain/model/entity/user.go
+++ domain/model/entity/user.go
@@ -1,13 +1,15 @@
 package entity
 
-import "jweb-notifier/domain/model/vo"
+import (
+	"jweb-notifier/domain/model/vo"
+)
 
 type User struct {
 	UserId   *vo.UserId
 	Password *vo.Password
 }
 
-func New(id *Id, password *Password) *User {
+func New(id *vo.UserId, password *vo.Password) *User {
 	return &User{
 		UserId:   id,
 		Password: password,
