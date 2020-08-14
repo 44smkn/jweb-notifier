058ad1b22266fdd3d6d4624037ec64197fc8f2d6
Kenji Shimizu <jrywm121@gmail.com>
2020-08-14 23:59:39 +0900

    voの条件を守る

--- domain/model/vo/diary_id.go
+++ domain/model/vo/diary_id.go
@@ -8,3 +8,17 @@ func NewDiaryId(id string) (*Id, error) {
 	// 桁数などでvalidataionをかける
 	return &Id{id}, nil
 }
+
+func (d *DiaryId) Equals(vo shared.ValueObject) bool {
+	if d == vo {
+		return true
+	}
+	if val, ok := vo.(DiaryId); !ok {
+		return false
+	}
+	return d.id == val
+}
+
+func (d *DiaryId) GetValue() {
+	return d.id
+}
