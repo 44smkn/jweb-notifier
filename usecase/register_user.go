af9024a9dbc59b3fddf77910b06cfffbe58a0a67
Kenji Shimizu <jrywm121@gmail.com>
2020-09-01 07:54:55 +0900

    entityのパッケージを変更したため呼び出し元も合わせて変更

--- usecase/register_user.go
+++ usecase/register_user.go
@@ -1,24 +1,30 @@
 package usecase
 
 import (
-	"jweb-notifier/domain/model/user"
+	"jweb-notifier/domain/model/entity"
+	"jweb-notifier/domain/model/repository"
+	"jweb-notifier/domain/model/vo"
 	"jweb-notifier/infrastructure/inmem"
 	"jweb-notifier/presentation/param"
 
 	"github.com/pkg/errors"
 )
 
-var repo user.Repository = &inmem.UserRepository{}
+var repo repository.User
+
+func init() {
+	repo = &inmem.UserRepository{}
+}
 
 func RegisterUser(u *param.User) error {
-	id, err := user.NewId(u.Id)
+	id, err := vo.NewUserId(u.Id)
 	if err != nil {
 		return errors.Wrapf(err, "creating vo of userid is fail. id: %s", u.Id)
 	}
-	password, err := user.NewPassword(u.Password)
+	password, err := vo.NewPassword(u.Password)
 	if err != nil {
 		return errors.Wrapf(err, "creating vo of userid is fail. password: %s", u.Password)
 	}
-	user := user.New(id, password)
+	user := entity.NewUser(id, password)
 	return repo.Register(user)
 }
