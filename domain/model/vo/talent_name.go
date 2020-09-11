dae9dd650a3c523a21cd3bb8020be7726f73a9a1
Kenji Shimizu <jrywm121@gmail.com>
2020-09-11 09:07:56 +0900

    constの生成メソッドを作成

--- domain/model/vo/talent_name.go
+++ domain/model/vo/talent_name.go
@@ -6,3 +6,14 @@ const (
 	KaitoTakahashi TalentName = "高橋海人"
 	NoeruKawashima TalentName = "川島如恵留"
 )
+
+// リフレクションを利用したほうが良いかも
+func TalentNameValueOf(name string) TalentName {
+	switch name {
+	case "高橋海人":
+		return KaitoTakahashi
+	case "川島如恵留":
+		return NoeruKawashima
+	}
+	return ""
+}
