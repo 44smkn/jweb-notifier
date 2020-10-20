2f8d9853e18aeb11a4a8f38d895644afa7f29339
Kenji Shimizu <jrywm121@gmail.com>
2020-10-21 08:18:28 +0900

    通知のusecaseを作成

--- usecase/notifier.go
+++ usecase/notifier.go
@@ -1,5 +1,24 @@
 package usecase
 
+import (
+	"errors"
+	du "jweb-notifier/domain/update"
+	"jweb-notifier/infrastructure/notification"
+)
+
+const NotificationBySlack = "slack"
+
 type Notifier interface {
 	Notify(string) error
+}
+
+func NotifyUpdates(upd du.Updates, notificationMethod string) error {
+	var notifier Notifier
+	switch notificationMethod {
+	case NotificationBySlack:
+		notifier = notification.NewSlackNotifier()
+	default:
+		return errors.New("Not exists method.")
+	}
+	return notifier.Notify()
 }
