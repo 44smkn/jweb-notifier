0998f6d95ccc2d0194a6c10478ca03c65dd685e5
Kenji Shimizu <jrywm121@gmail.com>
2020-10-22 00:36:46 +0900

    通知に本文を追加

--- usecase/notifier.go
+++ usecase/notifier.go
@@ -2,8 +2,10 @@ package usecase
 
 import (
 	"errors"
+	"fmt"
 	du "jweb-notifier/domain/update"
 	"jweb-notifier/infrastructure/notification"
+	"time"
 )
 
 const NotificationBySlack = "slack"
@@ -16,9 +18,12 @@ func NotifyUpdates(upd du.Updates, notificationMethod string) error {
 	var notifier Notifier
 	switch notificationMethod {
 	case NotificationBySlack:
-		notifier = notification.NewSlackNotifier()
+		// TODO: 設定からURLを注入できるようにするか取得処理をいれる
+		notifier = notification.NewSlackNotifier("webhook")
 	default:
 		return errors.New("Not exists method.")
 	}
-	return notifier.Notify()
+	header := `更新日時： %v
+あなたのお気に入りのブログに更新がありました！`
+	return notifier.Notify(fmt.Sprintf(header, upd.GetDateTime(time.RFC822)))
 }
